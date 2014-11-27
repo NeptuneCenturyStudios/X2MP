da91041d0dab331b12c49f3f882c3259843db3b0
Eric Butler <neptunecentury@gmail.com>
2014-11-27 10:18:21 -0500

    initial commit

---
+++ X2MP.Core/SoundEngine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X2MP.System
+{
+    public class SoundEngine : IDisposable
+    {
+        /// <summary>
+        /// Start the FMOD sound system engine
+        /// </summary>
+        public SoundEngine()
+        {
+
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ X2MP/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+//using X2MP.System;
+
+namespace X2MP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Gets the static reference to the sound engine (FMOD)
+        /// </summary>
+        //public static SoundEngine SoundEngine { get; private set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public App()
+        {
+            //SoundEngine = new SoundEngine();
+        }
+    }
+}
