47d26066cfbd4bf4b797d4c470cc12e748405e41
Eric Butler <neptunecentury@hotmail.com>
2015-06-03 16:49:28 -0400

    added ShellLibrary from Windows API code pack.

--- X2MP/Models/CollectionViewModel.cs
+++ X2MP/Models/CollectionViewModel.cs
@@ -4,18 +4,33 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.IsolatedStorage;
 using X2MP.Core;
+using Microsoft.WindowsAPICodePack.Shell;
+using System.Diagnostics;
 
 namespace X2MP.Models
 {
     public class CollectionViewModel : BaseViewModel
     {
-
+        #region Properties
         public ObservableCollection<PlayListEntry> Collection { get; set; }
+        #endregion
+
+        #region Constructor
         public CollectionViewModel()
         {
             Collection = new ObservableCollection<PlayListEntry>();
+
+            //var musicPath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            var sl = ShellLibrary.Load(KnownFolders.MusicLibrary, true);
+            foreach (var l in sl)
+            {
+                Debug.WriteLine(l.Path);
+            }
         }
+
+        #endregion
 
     }
 }
